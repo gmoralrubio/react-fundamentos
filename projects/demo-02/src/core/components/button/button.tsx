cd2ab3e4bf45a21ffc28f3503aad3999ab92d12d
guille <gmoralrubio@gmail.com>
2026-05-28 20:06:17 +0200

    style: añadidos comentarios

--- projects/demo-02/src/core/components/button/button.tsx
+++ projects/demo-02/src/core/components/button/button.tsx
@@ -1,25 +1,37 @@
-import type { MouseEvent, MouseEventHandler } from "react";
+import type { MouseEvent, MouseEventHandler } from 'react'
 import './button.css'
 
 type Props = {
-    readonly children: string;
-    readonly className?: string;
-    onClick: MouseEventHandler<HTMLButtonElement>;
-    [key: string]: unknown
-};
+  readonly children: string
+  readonly className?: string
+  onClick: MouseEventHandler<HTMLButtonElement>
+  [key: string]: unknown
+}
 
-export const Button: React.FC<Props> = ({ children, className, onClick, ...restOfProps }) => {
+// En restOfProps se guardan el resto de atributos que le pasamos a Button
+export const Button: React.FC<Props> = ({
+  children,
+  className,
+  onClick,
+  ...restOfProps
+}) => {
+  const totalClass = className ? 'button ' + className : 'button'
 
-    const totalClass = className ? 'button ' + className : 'button'
+  const handleClick = (
+    event: MouseEvent<HTMLButtonElement>,
+    id: number
+  ): void => {
+    console.log('Click en el Componente Button', id)
+    onClick(event)
+  }
 
-    const handleClick = (event: MouseEvent<HTMLButtonElement>, id: number): void => {
-        console.log("Click en el Componente Button", id)
-        onClick(event)
-    }
-
-    return (
-        <button className={totalClass} onClick={(event) => handleClick(event, 12) } {...restOfProps}>
-            {children}
-        </button>
-    );
-};
+  return (
+    <button
+      className={totalClass}
+      onClick={(event) => handleClick(event, 12)}
+      {...restOfProps}
+    >
+      {children}
+    </button>
+  )
+}
